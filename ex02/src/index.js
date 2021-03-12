390570172cebfce748fe5cdea29003b1bf639226
unknown <edina.kurtic90@hotmail.com>
2021-03-13 00:28:48 +0100

    Ex02 quoting strings with single quotes, edited

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,6 +1,6 @@
 function main() {
     // Only change code bellow this line
-    myLink = '<a href="http://www.example.com"target="_blank">Link</a>';
+    var myLink = '<a href="http://www.example.com"target="_blank">Link</a>';
 
     // Only change code above this line
     return myLink;
