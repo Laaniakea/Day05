2092924f9d0b8b4e5ce57e44a3a0085388cd375d
unknown <edina.kurtic90@hotmail.com>
2021-03-13 02:57:20 +0100

    ex15 was added,Word blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code bellow this line
+    var myNoun = "dog";
+    var myVerb = "run";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+
+    var wordBlanks = "A " + myAdjective + " " + myNoun + " " + myAdverb + " " + myVerb + "'s around Mistral" + ".";
+
+    // Only change code above this line
+    return wordBlanks;
+}
+console.log(main());
+module.exports = main;
