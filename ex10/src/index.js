b9497350ef94f3aa5589dccff5d6eaeb604f63e3
unknown <edina.kurtic90@hotmail.com>
2021-03-13 10:56:59 +0100

    fix sintax

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -2,7 +2,7 @@ function main() {
     // Only change code bellow this line
 
     var myString = "Jello World";
-    myString = "Hello World"
+    myString = "Hello World";
 
 
     // Only change code above this line
